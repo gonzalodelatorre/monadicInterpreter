ddc6f300cdae26ac2868d534e7a2581a33b9b60f
Gonzalo de Latorre <63663341+gonzalodelatorre@users.noreply.github.com>
2021-04-15 11:28:01 -0300

    Update Monads.hs

--- Monads.hs
+++ Monads.hs
@@ -9,7 +9,6 @@ class Monad m => MonadState m where
     lookfor :: Variable -> m Int
     update :: Variable -> Int -> m ()
 
--- Clase para representar mónadas que lanzan errores
 class Monad m => MonadError m where
     -- Throws an error
     throw :: Error -> m a
